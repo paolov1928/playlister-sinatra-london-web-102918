8089b045992a7cbc194c4be36a76603b5c0fa4ff
paolov1928 <ventura.paolo@gmail.com>
2018-11-22 16:20:34 +0000

    Build song controller ...made each song click-able

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,10 @@ class SongsController < ApplicationController
     erb :"songs/index"
   end
 
-
+  get "/songs/:id" do
+    # binding.pry
+    @song_selected = Song.find(params["id"])
+    erb :"songs/show"
+  end
 
 end
